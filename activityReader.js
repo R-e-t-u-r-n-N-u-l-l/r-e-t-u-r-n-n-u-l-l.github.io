499f64b7f911fd56282592bfed73641bbbe9b535
Return Null <0returnnull0@gmail.com>
2018-01-05 16:59:16 +0100

    1

--- activityReader.js
+++ activityReader.js
@@ -1,12 +1,15 @@
-function getUpdates() {
-  return searchActivity(["algorithm"]);
+function search() {
+  var string = document.getElementById("searchbar").value;
+
+  searchActivity(string.split(" "));
 }
 
 function getActivity(tags) {
   var activity = JSON.parse(JSON.stringify(activityFeed)).content.reverse();
 
   if(tags != undefined && tags.length > 0) {
-    for(var a of activity) {
+    for(var i = activity.length - 1; i >= 0; i--) {
+      var a = activity[i];
       for(var t of tags) {
         if(a.tags.indexOf(t) == -1)
           activity.splice(activity.indexOf(a), 1);
@@ -21,15 +24,29 @@ function searchActivity(keywords) {
   var activity = JSON.parse(JSON.stringify(activityFeed)).content.reverse();
 
   if(keywords != undefined && keywords.length > 0) {
-    for(var a of activity) {
+    for(var i = activity.length - 1; i >= 0; i--) {
+      var a = activity[i];
       var title = a.title.split(" ");
       var desc  = a.desc.split(" ");
       for(var k of keywords) {
-        if(title.indexOf(k) == -1 && desc.indexOf(k) == -1)
-          activity.splice(activity.indexOf(a), 1);
+        if(!contains(k, title) && !contains(k, desc))
+          activity.splice(i, 1);
       }
     }
   }
 
   return activity;
+}
+
+function contains(part, array) {
+  for(var a of array) {
+    var string = "";
+    for(var b of a) {
+      string += b;
+      if(string == part)
+        return true;
+    }
+  }
+
+  return false;
 }
