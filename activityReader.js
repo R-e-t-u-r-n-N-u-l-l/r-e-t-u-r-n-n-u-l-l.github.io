ccd228721bc4683fdf89e8fb3409173867f84fea
Return Null <0returnnull0@gmail.com>
2018-01-05 16:06:58 +0100

    1

--- activityReader.js
+++ activityReader.js
@@ -1,16 +1,31 @@
 function getUpdates() {
-  return getActivity();
+  return searchActivity(["algorithm"]);
 }
 
 function getActivity(tags) {
   var activity = JSON.parse(JSON.stringify(activityFeed)).content.reverse();
 
   if(tags != undefined && tags.length > 0) {
-    loop1:
     for(var a of activity) {
-      loop2:
       for(var t of tags) {
         if(a.tags.indexOf(t) == -1)
+          activity.splice(activity.indexOf(a), 1);
+      }
+    }
+  }
+
+  return activity;
+}
+
+function searchActivity(keywords) {
+  var activity = JSON.parse(JSON.stringify(activityFeed)).content.reverse();
+
+  if(keywords != undefined && keywords.length > 0) {
+    for(var a of activity) {
+      var title = a.title.split(" ");
+      var desc  = a.desc.split(" ");
+      for(var k of keywords) {
+        if(title.indexOf(k) == -1 && desc.indexOf(k) == -1)
           activity.splice(activity.indexOf(a), 1);
       }
     }
