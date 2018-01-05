0f6651664a0f29f82dfb45832df2c0c4ccd297bf
Return Null <0returnnull0@gmail.com>
2018-01-05 14:51:53 +0100

    1

--- activityReader.js
+++ activityReader.js
@@ -1,5 +1,20 @@
-function readActivity(index) {
-  var activity = JSON.parse(JSON.stringify(activityFeed));
+function getUpdates() {
+  return getActivity();
+}
+
+function getActivity(tags) {
+  var activity = JSON.parse(JSON.stringify(activityFeed)).content.reverse();
+
+  if(tags != undefined && tags.length > 0) {
+    loop1:
+    for(var a of activity) {
+      loop2:
+      for(var t of tags) {
+        if(a.tags.indexOf(t) == -1)
+          activity.splice(activity.indexOf(a), 1);
+      }
+    }
+  }
+
   return activity;
 }
-console.log(readActivity());
