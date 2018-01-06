c29540f9e5e7a5493b87dfcddb0875268d9361ca
Return Null <0returnnull0@gmail.com>
2018-01-06 23:30:01 +0100

    1

--- indexRender.js
+++ indexRender.js
@@ -1,8 +1,12 @@
 var offset 	= 0;
+
+// ratio : height / width
+var ratio   = 9 / 16;
+
 function initWebGL() {
   var container  = document.querySelector('#canvas_container');
   var width      = getWidth();
-	webGL 		     = new WebGL([width, width], container);
+	webGL 		     = new WebGL([width, width * ratio], container);
   resizeWebGL();
 	webGL.camera.location = new Vector3D(0, 0, -5);
 	webGL.useShader(SHADERS.PER_FRAGMENT_LIGHTING);
@@ -19,20 +23,22 @@ function draw() {
   Lighting.AmbientLight('#000');
 	Lighting.PointLight('#2f2', '#dfd', new Vector3D(0, 0, 10))
 	custom.rotate(0, offset / 1.5, 0);
-	custom.scale(2);
+	custom.scale(2, 2.5, 2);
 	custom.location.y = Math.sin(offset / 3);
 	custom.render();
 	offset += 0.03;
 	requestAnimationFrame(draw);
 }
 
+
 function getWidth() {
   var container = document.querySelector('#canvas_container');
   return parseInt(container.getBoundingClientRect().width);
 }
+
 function resizeWebGL() {
   webGL.canvas.width  = getWidth();
-  webGL.canvas.height = getWidth();
+  webGL.canvas.height = getWidth() * ratio;
   webGL.context.viewport(0, 0, webGL.canvas.width, webGL.canvas.height);
   WEBGL_SETTINGS.RATIO = getWidth() / getWidth();
 }
